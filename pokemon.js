83d36c58ff0168f3e4f0d9f8b29d7291b515a72c
josemcnieto <josemcnieto@gmail.com>
2026-05-28 21:44:04 +0200

    añadidos js

--- pokemon.js
+++ pokemon.js
@@ -43,6 +43,32 @@ btn.addEventListener("click", async () => {
         out.textContent = "❌ Pokémon no encontrado";
     }
     
+
+    const estadisticasHTML = pokemon.stats.map(stat => {
+    return `
+        <li>
+            <strong>${stat.stat.name.toUpperCase()}:</strong>
+            ${stat.base_stat}
+        </li>
+    `;
+}).join("");
+
+out.innerHTML = `
+    <h2>${pokemon.name.toUpperCase()} (#${pokemon.id})</h2>
+
+    <img 
+        src="${pokemon.sprites.front_default}" 
+        alt="${pokemon.name}"
+    >
+
+    <p><strong>Tipos:</strong> ${tipos}</p>
+
+    <ul>
+        ${estadisticasHTML}
+    </ul>
+`;
+
+
 });
 
 // reproducir sonido
